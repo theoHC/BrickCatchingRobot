b72f4ed03eb9f54964e55535c334a05344e000db
theoHC <theo_coulson@me.com>
2025-10-08 18:25:23 -0500

    Gitignore and stuff

--- turtle_brick/setup.py
+++ turtle_brick/setup.py
@@ -9,7 +9,10 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml',
+                                   'launch/see_robot.launch.xml',
+                                   'urdf/turtle_bot.urdf',
+                                   'config/peruse_turtle.rviz']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
