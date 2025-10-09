31b8529d9ec860fd5cac5b5e6c0b0c23f66216fd
theoHC <theo_coulson@me.com>
2025-10-09 12:51:53 -0500

    Robot moves when controlled via cmd_vel

---
+++ turtle_brick/turtle_brick/turtle_robot.py
@@ -0,0 +1,65 @@
+from math import pi
+
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import Vector3
+import rclpy
+from rclpy.node import Node
+from tf2_ros import TransformBroadcaster
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+from turtlesim_msgs.msg import Pose
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    turtle_robot = TurtleRobot()
+    
+    turtle_robot.get_logger().info('turtle_robot node initializing')
+
+    rclpy.spin(turtle_robot)
+    
+    turtle_robot.destroy_node()
+
+    rclpy.shutdown()
+
+class TurtleRobot(Node):
+    """Uses the turtlesim node to control the location of a robot
+    """
+
+    def __init__(self):
+        super().__init__('turtle_robot')
+
+        self.special_callback = MutuallyExclusiveCallbackGroup()
+
+        self.static_broadcaster = StaticTransformBroadcaster(self)
+
+        self.broadcaster = TransformBroadcaster(self)
+
+        self.turtle_listener = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
+
+        self.setup_odom = False
+
+    def pose_callback(self, pose):
+        time = self.get_clock().now().to_msg()
+        if not self.setup_odom:
+            self.setup_odom = True
+            world_odom_tf = TransformStamped()
+            world_odom_tf.header.frame_id = 'world'
+            world_odom_tf.child_frame_id = 'odom'
+            world_odom_tf.header.stamp = time
+            world_odom_tf.transform.translation = Vector3(x=pose.x, y=pose.y, z=0.0)
+            self.static_broadcaster.sendTransform(world_odom_tf)
+
+            self.startloc = (pose.x, pose.y)
+        
+        odom_bot_tf = TransformStamped()
+        odom_bot_tf.header.stamp = time
+        odom_bot_tf.header.frame_id = 'odom'
+        odom_bot_tf.child_frame_id = 'base_link'
+        odom_bot_tf.transform.translation=Vector3(x=pose.x - self.startloc[0], y=pose.y - self.startloc[1], z=0.0)
+
+        self.broadcaster.sendTransform(odom_bot_tf)
+
