87703f14f780197b1f0f4b9bfed4551bb851c5cc
theoHC <theo_coulson@me.com>
2025-10-23 13:26:49 -0500

    Mild improvement

--- turtle_brick/test/test_brick_launch.py
+++ turtle_brick/test/test_brick_launch.py
@@ -55,12 +55,13 @@ class TestCMDVelFrequency(unittest.TestCase):
         """Test the frequency at which cmd_vel messages are published."""
         start_time = self.node.get_clock().now()
 
-        while self.node.get_clock().now() - start_time < rclpy.duration.Duration(seconds=10.0):
+        duration = 10.0
+        while self.node.get_clock().now() - start_time < rclpy.duration.Duration(seconds=duration):
             rclpy.spin_once(self.node)
 
         self.node.get_logger().info(f'received messages: {self.received_msgs}')
 
-        assert abs(self.received_msgs - 1000) < 100
+        assert abs(self.received_msgs - 100 * duration) < 100
 
     def cmd_vel_callback(self, msg):
         self.received_msgs += 1
